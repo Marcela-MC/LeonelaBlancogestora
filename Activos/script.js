f02c8f07583495e56ca490d8b9a3175b06512d8b
Marcela-MC <marcelamongecalvo@gmail.com>
2025-07-24 12:47:53 -0600

    Create script.js

---
+++ Activos/script.js
@@ -0,0 +1,7 @@
+
+const toggleBtn = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.nav-list');
+
+toggleBtn.addEventListener('click', () => {
+  navList.classList.toggle('show');
+});
